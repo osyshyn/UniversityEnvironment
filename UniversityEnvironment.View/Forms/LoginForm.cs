1c721c17bb540ced17dc9dd00bc8199fe0431cf9
osyshyn <129138229+osyshyn@users.noreply.github.com>
2024-04-30 08:51:23 +0300

    Refactoring LoginForm.cs

--- UniversityEnvironment.View/Forms/LoginForm.cs
+++ UniversityEnvironment.View/Forms/LoginForm.cs
@@ -39,64 +39,37 @@ namespace UniversityEnvironment.View.Forms
             registrationWindow.Show();
         }
 
+        private T SetRole<T>() where T : User
+        {
+            var role = typeof(T).Name;
+            var user = RepositoryManager.GetRepo<T>()
+                    .FindByFilter(u => u.Username == UsernameTextBox.Text && u.Password == PasswordTextBox.Text);
+
+            user.Role = role;
+            return user;
+        }
+
+        private void ShowNextForm(MaterialForm current, MaterialForm next) where TNext : MaterialForm
+        {
+            current.Hide();
+            next.FormClosed += (s, arg) => current.Show();
+            next.Show();
+        }
+
         private void LoginButton_Click(object sender, EventArgs e)
         {
-            User actualUser = new User();
-            if (AdminCheck.Checked)
-            {
-                var ourAdmin = RepositoryManager.GetRepo<User>()
-                    .FindByFilter(u => u.Username == UsernameTextBox.Text && u.Password == PasswordTextBox.Text);
-                if (ourAdmin != null)
-                {
-                    actualUser.Id = ourAdmin.Id;
-                    actualUser.Username = ourAdmin.Username;
-                    actualUser.FirstName = ourAdmin.FirstName;
-                    actualUser.LastName = ourAdmin.LastName;
-                    actualUser.Password = ourAdmin.Password;
-                    actualUser.Courses = ourAdmin.Courses;
-                    actualUser.Role = "Admin";
-                }
-            }
+            User user;
+            if (Admin.Checked)
+                user = SetRole<Admin>();
             else if (TeacherCheck.Checked)
-            {
-                var ourTeacher = RepositoryManager.GetRepo<Teacher>()
-                    .FindByFilter(u => u.Username == UsernameTextBox.Text && u.Password == PasswordTextBox.Text);
-                if (ourTeacher != null)
-                {
-                    actualUser.Id = ourTeacher.Id;
-                    actualUser.Username = ourTeacher.Username;
-                    actualUser.FirstName = ourTeacher.FirstName;
-                    actualUser.LastName = ourTeacher.LastName;
-                    actualUser.Password = ourTeacher.Password;
-                    actualUser.Courses = ourTeacher.Courses;
-                    actualUser.Role = "Teacher";
-                }
-            }
+                user = SetRole<Teacher>();
             else
-            {
-                var ourStudent = RepositoryManager.GetRepo<Student>()
-                    .FindByFilter(u => u.Username == UsernameTextBox.Text && u.Password == PasswordTextBox.Text);
-                if (ourStudent != null)
-                {
-                    actualUser.Id = ourStudent.Id;
-                    actualUser.Username = ourStudent.Username;
-                    actualUser.FirstName = ourStudent.FirstName;
-                    actualUser.LastName = ourStudent.LastName;
-                    actualUser.Password = ourStudent.Password;
-                    actualUser.Courses = ourStudent.Courses;
-                    actualUser.Role = "Student";
-                }
-            }
+                user = SetRole<Student>();
 
-            if (actualUser.Role != null)
+            if (user.Role != null)
             {
-                Hide();
-                EnvironmentForm ViewForm = new EnvironmentForm(actualUser);
-                ViewForm.FormClosed += (s, arg) =>
-                {
-                    Show();
-                };
-                ViewForm.Show();
+                ShowNextForm(this, new EnvironmentForm(user));
+               
                 return;
             }
             MessageBox.Show("Wrong username or password...", "Login", MessageBoxButtons.OK);
